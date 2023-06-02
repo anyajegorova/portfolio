34d4e11c2c7a0a463bd95a11b08caa2002cc2768
anyajegorova <anna.jerm@outlook.com>
2023-06-02 20:34:48 +0300

    added Axure2 prototype pictures in UX/UI tab

--- profile_client/src/views/UXUIDesign.js
+++ profile_client/src/views/UXUIDesign.js
@@ -23,6 +23,13 @@ import Quizzes from '../images/UXUI/Axure/QuizzesAnsGames.png';
 import Scores from '../images/UXUI/Axure/Scores.png';
 import SignUpAxure from '../images/UXUI/Axure/SignUp.png';
 
+import CalendarAxure from '../images/UXUI/Axure2/axure_calendar.png';
+import Check from '../images/UXUI/Axure2/axure_check.png';
+import Continue from '../images/UXUI/Axure2/axure_continue.png';
+import Final from '../images/UXUI/Axure2/axure_final.png';
+import Home from '../images/UXUI/Axure2/axure_home.png';
+import Next from '../images/UXUI/Axure2/axure_next.png';
+
 import Divider from '@mui/material/Divider';
 import Button from '@mui/material/Button';
 
@@ -30,14 +37,15 @@ const UXUIDesign = () => {
 
     const marvelImages = [SignUp, Messages, Group, Chat, Keyboard];
     const axureImages = [SignUpAxure, LogIn, Calendar, Materials, Notifications, MessagesAxure, Quizzes, Scores];
+    const axureImages2 = [Home, Check, Next, CalendarAxure, Continue, Final];
     const InvisionAppImages = [SignUpInvision, University, Restaurants, Remove, Events, Destinations, Profile];
 
     return (
         <div className='uxui-container'>
             <p class='naming-paragraph'>UX/UI Design</p>
-            <Divider variant="middle" color='white' style={{ margin: 20 }} />
+            {/* <Divider variant="middle" color='white' style={{ margin: 20 }} />
             <cite>"These UI designs were created during User Experience and Prototyping of Digital Services courses. The following page includes the screenshots of parts that were designed by me. If you wish to look at the full designs, click 'OPEN PROTOTYPE' button." </cite>
-            <Divider variant="middle" color='white' style={{ margin: 20 }} />
+            <Divider variant="middle" color='white' style={{ margin: 20 }} /> */}
             <div className='prototype-container'>
 
                 <div className='prototypes'>
@@ -108,6 +116,42 @@ const UXUIDesign = () => {
                         </table>
 
                         <Button variant="outlined" style={{ borderRadius: 20, borderColor: '#fff', color: '#fff', maxWidth: '50%', alignSelf: 'center' }} onClick={() => window.open('')} sx={{ ':hover': { bgcolor: 'rgba(253, 251, 251, 0.255)' } }}>OPEN PROTOTYPE</Button>
+                    </div>
+
+                </div>
+                <div className='prototypes'>
+                    <div className='slider'>
+                        <SimpleImageSlider
+                            width={308}
+                            height={548}
+                            images={axureImages2}
+                            showNavs={true}
+                        /></div>
+                    <div className='prototype-description'>
+                        <table>
+                            <tr>
+                                <td>Tool:</td>
+                                <td style={{ color: '#ff5e00', fontWeight: 'bold' }}>Axure</td>
+                            </tr>
+                            <tr>
+                                <td>Done by:</td>
+                                <td>team of three students</td>
+                            </tr>
+                            <tr>
+                                <td>About:</td>
+                                <td>Design a reliable app for Digital Services students of Haaga-Helia</td>
+                            </tr>
+                            <tr>
+                                <td>Goals:</td>
+                                <td> easy access to course materials, easy-to-use and intuitive interface</td>
+                            </tr>
+                            <tr>
+                                <td>Methods:</td>
+                                <td>survey, user profile, interview, Task Matrics, scenario, paper prototype</td>
+                            </tr>
+                        </table>
+
+                        <Button variant="outlined" style={{ borderRadius: 20, borderColor: '#fff', color: '#fff', maxWidth: '50%', alignSelf: 'center' }} onClick={() => window.open('https://onvtee.axshare.com')} sx={{ ':hover': { bgcolor: 'rgba(253, 251, 251, 0.255)' } }}>OPEN PROTOTYPE</Button>
                     </div>
 
                 </div>
